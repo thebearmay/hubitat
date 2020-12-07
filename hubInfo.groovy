64cb4b82e56b91312d740ebea92819d8d1378df4
thebearmay <32334899+thebearmay@users.noreply.github.com>
2020-12-07 14:26:00 -0500

    Create hubInfo.groovy

---
+++ hubInfo.groovy
@@ -0,0 +1,62 @@
+ /*
+ * Hub Info
+ *
+ *  Licensed Virtual the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ *  Change History:
+ *
+ *    Date        Who            What
+ *    ----        ---            ----
+ *    2020-12-07  thebearmay	 Original version 0.1.0
+ * 
+ */
+
+static String version()	{  return '0.1.0'  }
+
+metadata {
+    definition (
+		name: "Hub Information", 
+		namespace: "thebearmay", 
+		author: "Jean P. May, Jr."
+	) {
+        	capability "Actuator"
+		
+		attribute "latitude", "string"
+		attribute "longitude", "string"
+        
+		command "configure", []
+            
+    }   
+}
+
+preferences {
+	input("debugEnable", "bool", title: "Enable debug logging?")
+}
+
+
+
+def installed() {
+	log.trace "installed()"
+}
+
+def configure() {
+    if(debugEnable) log.debug "configure()"
+    sendEvent(name:"latitude", value:location.getLatitude())
+	sendEvent(name:"longitude", value:location.getLongitude())
+}
+
+def updated(){
+	log.trace "updated()"
+	if(debugEnable) runIn(1800,logsOff)
+}
+
+void logsOff(){
+     device.updateSetting("debugEnable",[value:"false",type:"bool"])
+}

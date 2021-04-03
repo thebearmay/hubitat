c484edbc146dcc44b20891d892b29aa4eac75c68
thebearmay <32334899+thebearmay@users.noreply.github.com>
2021-04-03 06:05:15 -0400

    Update nodeRedArlo.groovy

--- nodeRedArlo.groovy
+++ nodeRedArlo.groovy
@@ -1,5 +1,5 @@
  /*
- *  Node Red Motion
+ *  Node Red Arlo Camera
  *
  *  Licensed Virtual the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:

07c465dfe4d23b0fe446c6fbf663fafde8265dc5
thebearmay <32334899+thebearmay@users.noreply.github.com>
2021-05-17 11:27:34 -0400

    Update stChildButton.groovy

--- development/stChildButton.groovy
+++ development/stChildButton.groovy
@@ -1,5 +1,7 @@
-/*
- * ST Child Button
+/**
+ *  Child Button
+ *
+ *  Copyright 2017 SmartThings
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
@@ -10,22 +12,23 @@
  *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
  *  for the specific language governing permissions and limitations under the License.
  *
- *  Interpretation of the ST Child Button
+ *  Ported from ST thebearmay
  */
 metadata {
-	  definition (name: "ST Child Button", namespace: "hubitat", author: "thebearmay") {
+	definition (name: "ST Child Button", namespace: "hubitat", author: "SmartThings", ocfDeviceType: "x.com.st.d.remotecontroller") {
 		capability "PushableButton"
 		capability "HoldableButton"
 		capability "Sensor"
         
-    attribute "supportedButtonValues", "ENUM"
+        attribute "supportedButtonValues", "ENUM"
+        attribute "button", "string"
+        attribute "numberOfButtons", "number"
 	}
 
 }
 
 def installed() {
 	sendEvent(name: "numberOfButtons", value: 1)
-    subscribe( device.getParent(), "supportedButtonValues", "btnValueHndler")
 }
 
 def push(buttonNumber){
@@ -34,8 +37,4 @@ def push(buttonNumber){
 
 def hold(buttonNumber){
     sendEvent(name:"held", value:buttonNumber)
-}
-
-def btnValueHndler(evt) {
-    sendEvent(name:"supportedButtonValues", value: evt.value)
 }

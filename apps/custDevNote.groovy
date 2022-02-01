61f3637d045b3a3b185888e034cbbe1cb08c0814
thebearmay <32334899+thebearmay@users.noreply.github.com>
2022-02-01 12:12:13 -0500

    Update custDevNote.groovy

--- apps/custDevNote.groovy
+++ apps/custDevNote.groovy
@@ -1,5 +1,18 @@
-/* Contact Sensor Close
-
+/* Device Custom Note
+ *
+ *  Licensed Virtual the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ *  Change History:
+ *
+ *    Date          Who           What
+ *    ----          ---           ----
  */
 
 static String version()	{  return '0.0.1'  }
@@ -9,7 +22,7 @@ definition (
 	name: 			"Custom Device Note", 
 	namespace: 		"thebearmay", 
 	author: 		"Jean P. May, Jr.",
-	description: 	"Force/Send close event with optional button.",
+	description: 	"Add a custom note to any device.",
 	category: 		"Utility",
 	importUrl: "https://raw.githubusercontent.com/thebearmay/hubitat/main/apps/custDevNote.groovy",
 	oauth: 			false,

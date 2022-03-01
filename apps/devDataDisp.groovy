ea9e37719ff0bd354ba2d6efa10103f4c0051a9e
thebearmay <32334899+thebearmay@users.noreply.github.com>
2022-03-01 14:39:54 -0500

    Create devDataDisp.groovy

---
+++ apps/devDataDisp.groovy
@@ -0,0 +1,117 @@
+/*
+ * Device Data Display
+ *
+ *  Licensed Virtual the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ *  Change History:
+ *
+ *    Date        Who           What
+ *    ----        ---           ----
+ */
+import java.text.SimpleDateFormat
+static String version()	{  return '1.0.0'  }
+
+
+definition (
+	name: 			"Device Data Display", 
+	namespace: 		"thebearmay", 
+	author: 		"Jean P. May, Jr.",
+	description: 	"Display the capabilities, attributes, commands and device data for devices selected.",
+	category: 		"Utility",
+	importUrl: "https://raw.githubusercontent.com/thebearmay/hubitat/main/apps/devDataDisp.groovy",
+	oauth: 			false,
+    iconUrl:        "",
+    iconX2Url:      ""
+) 
+
+preferences {
+   page name: "mainPage"
+   page name: "deviceData"
+
+}
+
+def installed() {
+//	log.trace "installed()"
+    state?.isInstalled = true
+    initialize()
+}
+
+def updated(){
+//	log.trace "updated()"
+    if(!state?.isInstalled) { state?.isInstalled = true }
+	if(debugEnable) runIn(1800,logsOff)
+}
+
+def initialize(){
+}
+
+void logsOff(){
+     app.updateSetting("debugEnable",[value:"false",type:"bool"])
+}
+
+def mainPage(){
+    dynamicPage (name: "mainPage", title: "", install: true, uninstall: true) {
+      	if (app.getInstallationState() == 'COMPLETE') {   
+	    	section("Main")
+		    {
+                input "qryDevice", "capability.*", title: "Devices of Interest:", multiple: true, required: true, submitOnChange: true
+                if (qryDevice != null) {
+                    dataList = buildDataList()
+                    input "varList", "enum", title: "Select data items to display", options: dataList, multiple: true, required: false, submitOnChange: true
+                    if(varList !=null)
+                        href "deviceData", title: "Display Data", required: false
+                }
+		    }
+	    } else {
+		    section("") {
+			    paragraph title: "Click Done", "Please click Done to install app before continuing"
+		    }
+	    }
+    }
+}
+
+def buildDataList(){
+    List dataList = []
+    qryDevice.each {
+        it.properties.data.each {
+            dataList.add(it.key)
+        }
+    }
+    dataList = dataList.sort().unique()
+    return dataList
+}
+
+def deviceData(){
+    dynamicPage (name: "deviceData", title: "", install: false, uninstall: false) {
+	  section("Device Data"){
+          qryDevice.each{ x->
+              paragraph "<p style='font-weight:bold;text-decoration:underline'>$x.displayName</p>"
+              varOut = ""
+              varList.each {
+                  if(x.properties.data["$it"]) varOut+= "$it: ${x.properties.data["$it"]}<br>"
+              }
+              paragraph varOut
+          }
+       }
+    }
+}
+
+
+def appButtonHandler(btn) {
+    switch(btn) {
+          default: 
+              log.error "Undefined button $btn pushed"
+              break
+      }
+}
+
+def intialize() {
+
+}

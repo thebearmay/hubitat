69f8a5d5b053458ccabd8d455d9707826deaaf86
thebearmay <32334899+thebearmay@users.noreply.github.com>
2022-03-09 10:49:25 -0500

    Update hubInfo.groovy

--- hubInfo.groovy
+++ hubInfo.groovy
@@ -87,12 +87,13 @@
  *    2022-01-17  thebearmay     allow reboot to be called without Hub Monitor parameter
  *    2022-01-21  thebearmay     add Mode and HSM Status as a pollable attribute
  *    2022-03-03  thebearmay     look at attribute size each poll and enforce 1024 limit
+ *    2022-03-09  thebearmay     fix lastUpdated not always updated
 */
 import java.text.SimpleDateFormat
 import groovy.json.JsonSlurper
 
 @SuppressWarnings('unused')
-static String version() {return "2.6.19"}
+static String version() {return "2.6.20"}
 
 metadata {
     definition (
@@ -652,6 +653,7 @@ void getPollValues(){
             runIn(tempPollRate,"getPollValues")
         }
     }
+    updateAttr("lastUpdated", now())
 }
 
 @SuppressWarnings('unused')

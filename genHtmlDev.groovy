cf38e9922b3af1dc88b640417db738d3715e7c10
thebearmay <32334899+thebearmay@users.noreply.github.com>
2022-04-02 06:41:09 -0400

    Create genHtmlDev.groovy

---
+++ genHtmlDev.groovy
@@ -0,0 +1,51 @@
+ /*
+ * Generic HTML Attribute 
+ *
+ *  Licensed Virtual the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ *  Change History:
+ *
+ *    Date        Who            What
+ *    ----        ---            ----
+ *    2021-08-03  thebearmay	 Original version 0.0.1
+ */
+
+static String version()	{  return '0.0.1'  }
+
+metadata {
+    definition (
+		name: "Generic HTML Device", 
+		namespace: "thebearmay", 
+		author: "Jean P. May, Jr.",
+	        importUrl:"https://raw.githubusercontent.com/thebearmay/hubitat/main/genHtmlDev.groovy"
+	) {
+        capability "Actuator"
+		
+		attribute "html", "string"
+
+    }   
+}
+
+preferences {
+	input("debugEnable", "bool", title: "Enable debug logging?")
+}
+
+def installed() {
+    log.trace "${device.properties.displayName} installed()"
+}
+
+def updated(){
+    log.trace "updated()"
+    if(debugEnable) runIn(1800,logsOff)
+}
+
+void logsOff(){
+     device.updateSetting("debugEnable",[value:"false",type:"bool"])
+}

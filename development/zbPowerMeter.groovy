55313c679c2d235a464c9fe99b774dcfe1da8c37
thebearmay <32334899+thebearmay@users.noreply.github.com>
2022-04-15 07:38:10 -0400

    Update zbPowerMeter.groovy

--- development/zbPowerMeter.groovy
+++ development/zbPowerMeter.groovy
@@ -1,4 +1,3 @@
-
 /**
  *  Copyright 2019 SmartThings
  *
@@ -26,6 +25,9 @@ metadata {
         capability "Sensor"
         capability "Configuration"
  //       capability "Power Consumption Report"
+	    
+	    attribute "powerConsumption", "JSON"
+        attribute "checkInterval", "NUMBER"
 
         fingerprint profileId: "0104", deviceId:"0053", inClusters: "0000, 0003, 0004, 0B04, 0702", outClusters: "0019", manufacturer: "", model: "E240-KR080Z0-HA", deviceJoinName: "Energy Monitor" //Smart Sub-meter(CT Type)
         fingerprint profileId: "0104", deviceId:"0007", inClusters: "0000,0003,0702", outClusters: "000A", manufacturer: "Develco", model: "ZHEMI101", deviceJoinName: "frient Energy Monitor" // frient External Meter Interface (develco) 02 0104 0007 00 03 0000 0003 0702 01 000A

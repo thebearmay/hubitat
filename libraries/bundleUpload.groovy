0518a3e2f5c53b059cad8718c943d3b36ef861ab
thebearmay <32334899+thebearmay@users.noreply.github.com>
2022-05-04 16:32:44 -0400

    Update bundleUpload.groovy

--- libraries/bundleUpload.groovy
+++ libraries/bundleUpload.groovy
@@ -2,25 +2,24 @@ Boolean installBundle(bundle) {
     //from the github make sure that the url ends with "?raw=true"
     if(security) cookie = securityLogin().cookie
     //adapted from BPTWorld code 
+    def jsonData =  JsonOutput.toJson([url:"$bundle",installer:FALSE, pwd:''])
     try {
         def params = [
             uri: 'http://127.0.0.1:8080/bundle/uploadZipFromUrl',
             headers: [
-                "Accept": '/',
-                "ContentType": 'text/html; charset=utf-8',
+                "Accept": '*/*',
+                "ContentType": 'text/plain; charset=utf-8',
                 "Cookie": cookie
             ],
-            body: bundle,
+            body: "$jsonData",
             timeout: 30,
             ignoreSSLIssues: true
         ]
+        log.debug "In installBundleHandler - Getting data ($params)"
         httpPost(params) { resp ->
-            log.debug "In installBundleHandler - Receiving file: ${bundle}"
-            log.debug "In installBundleHandler - Hopefully installing Bundle"
+             log.debug "In installBundleHandler - Receiving file: ${bundle}"
         }
-        return true
-    } catch (e) {
+   } catch (e) {
         log.error(getExceptionMessageWithLine(e))
-        return false
-    }
+   }
 }

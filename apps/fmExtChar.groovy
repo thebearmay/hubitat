f1de1769c292d9c6ef596b43c9b661f14c11f80a
thebearmay <32334899+thebearmay@users.noreply.github.com>
2022-05-25 15:35:31 -0400

    Update fmExtChar.groovy

--- apps/fmExtChar.groovy
+++ apps/fmExtChar.groovy
@@ -17,7 +17,7 @@
  */
 
 import java.text.SimpleDateFormat
-static String version()	{  return '0.0.1'  }
+static String version()	{  return '0.0.2'  }
 
 
 definition (
@@ -26,7 +26,7 @@ definition (
 	author: 		"Jean P. May, Jr.",
 	description: 	"Overcomes the Extended Character display issue with File Manager and allows download of the selected file.",
 	category: 		"Utility",
-	importUrl: "https://raw.githubusercontent.com/thebearmay/hubitat/main/apps/fmExtChar.groovy",
+	importUrl: "https://raw.githubusercontent.com/thebearmay/hubitat/main/apps/xxxx.groovy",
 	oauth: 			false,
     iconUrl:        "",
     iconX2Url:      ""
@@ -34,7 +34,7 @@ definition (
 
 preferences {
    page name: "mainPage"
-   page name: "fileDispDown"
+   page name: "fileDisp", nextPage: "mainPage"
 
 }
 
@@ -65,20 +65,24 @@ def mainPage(){
               section("File List", hideable: true, hidden: false){
                   fileList = listFiles()
                   SimpleDateFormat sdf= new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
-                  oData = "<table><tr><th style='text-align:center;width:20em'>Name</th><th style='text-align:center;width:2em'>Size</th><th style='width:3em'></th><th style='text-align:center;width:10em'>Date</th></tr>"
+                  paragraph "<div><div style='width:15em'>&nbsp;</div><div style='text-align:center;width:26em;float:left;font-weight:bold'>Name</div><div style='text-align:center;width:5em;float:left;font-weight:bold'>Size</div><div style='text-align:center;width:10em;float:left;font-weight:bold'>Date</div></div>"
+                  
                   fileList.each {
                       if(it.date)                   
                           dDate = sdf.format(new Date(Long.parseLong(it.date)))
                       else
                           dDate = "Date not Available"
-                      oData += "<tr><td><a href='http://${location.hub.localIP}:8080/local/${it.name}?raw=true'>${it.name}</a></td>"
-                      oData += "<td style='text-align:right'>${it.size}</td><td></td>"
-                      oData += "<td>$dDate</td></tr>"
+                      oData =  "<div><div style='float:left;width:6em'><button><a href='http://${location.hub.localIP}:8080/local/${it.name}' target='_blank'>FM Classic</a></button></div>"
+                      oData +="<div style='width:20em;float:left'><a href='http://${location.hub.localIP}:8080/local/${it.name}?raw=true'>${it.name}</a></div>"
+                      oData +="<div style='width:5em;float:left;text-align:right'>${String.format('%,.0f',it.size.toDouble())}</div>"
+                      oData +="<div style='width:10em;float:left;text-align:right'>$dDate</div></div>"
+                      paragraph oData
+                      href "fileDisp", title: "View" , description: "", params:["fName": "${it.name}"], width:2
                   }
-                  oData += "</table>"
-                  paragraph oData
-                  input "debugEnabled", "bool", title: "Enable Debug Logging", required: false, defaultValue:false, submitOnChange:true
-                  input("security", "bool", title: "Hub Security Enabled", defaultValue: false, submitOnChange: true)
+
+                  paragraph "<hr />"
+                  input "debugEnabled", "bool", title: "Enable Debug Logging", required: false, defaultValue:false, submitOnChange:true, width:4
+                  input "security", "bool", title: "Hub Security Enabled", defaultValue: false, submitOnChange: true, width:4
                   if (security) { 
                     input("username", "string", title: "Hub Security Username", required: false)
                     input("password", "password", title: "Hub Security Password", required: false)
@@ -101,8 +105,16 @@ def mainPage(){
     }
 }
 
+def fileDisp(params){
+    dynamicPage (name: "fileDisp", title: "File Display", nextPage: "mainPage", install: false, uninstall:false) {
+        section("File Contents") {
+            paragraph readFile("$params.fName")            
+        }
+    }
+}
+
 def appButtonHandler(btn) {
-    switch(btn) {
+    switch(btn) {       
         default: 
             log.error "Undefined button $btn pushed"
             break
@@ -114,10 +126,7 @@ def intialize() {
 }
 
 void uninstalled(){
-    if(getChildDevice("ddd${app.id}-01")){
-        unsubscribe()
-        deleteChildDevice("ddd${app.id}-01")
-    }          
+        
 }
 
 HashMap securityLogin(){
@@ -158,6 +167,45 @@ HashMap securityLogin(){
 	return [result: result, cookie: cookie]
 }
 
+String readFile(fName){
+    if(security) cookie = securityLogin().cookie
+    uri = "http://${location.hub.localIP}:8080/local/${fName}"
+
+
+    def params = [
+        uri: uri,
+        contentType: "text/html",
+        textParser: true,
+        headers: [
+				"Cookie": cookie,
+                "Accept": "application/octet-stream"
+            ]
+    ]
+
+    try {
+        httpGet(params) { resp ->
+            if(resp!= null) {       
+               int i = 0
+               String delim = ""
+               i = resp.data.read() 
+               while (i != -1){
+                   char c = (char) i
+                   delim+=c
+                   i = resp.data.read() 
+               }
+               if(logResponses) log.info "File Read Data: $delim"
+               return delim
+            }
+            else {
+                log.error "Null Response"
+            }
+        }
+    } catch (exception) {
+        log.error "Read Error: ${exception.message}"
+        return null
+    }
+}    
+    
 List<String> listFiles(){
      if(security) cookie = securityLogin().cookie
     // Adapted from BptWorld's Community Post 89466/4

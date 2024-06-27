13855ad74d58539446d2339a7f642cf933d2c788
thebearmay <32334899+thebearmay@users.noreply.github.com>
2024-06-27 15:17:28 -0400

    Create devAttIterStoUI.groovy

---
+++ apps/devAttIterStoUI.groovy
@@ -0,0 +1,184 @@
+/*
+ *
+ *  Licensed Virtual the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WIyTHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ *  Change History:
+ *
+ *    Date          Who          Description
+ *    ----------   ------------  ------------------------------------------------
+ *    27Jun2024    thebearmay    Original Code
+ */
+    
+
+
+static String version()	{  return '0.0.1'  }
+
+//import groovy.json.JsonSlurper
+//import groovy.json.JsonOutput
+//import groovy.transform.Field
+
+
+definition (
+	name: 			"Device Attribute Iterative Storage - UI", 
+	namespace: 		"thebearmay", 
+	author: 		"Jean P. May, Jr.",
+	description: 	"User Interface/Parent for Device Attribute Iterative Storage",
+	category: 		"Utility",
+	importUrl: "https://raw.githubusercontent.com/thebearmay/hubitat/main/apps/devAttIterStoUI.groovy",
+    installOnOpen:  true,
+	oauth: 			false,
+    iconUrl:        "",
+    iconX2Url:      ""
+) 
+
+preferences {
+    page name: "mainPage"
+
+}
+mappings {
+
+}
+
+def installed() {
+//	log.trace "installed()"
+    state?.isInstalled = true
+    initialize()
+}
+
+def updated(){
+//	log.trace "updated()"
+    if(!state?.isInstalled) { state?.isInstalled = true }
+	if(debugEnabled) runIn(1800,logsOff)
+}
+
+def initialize(){
+}
+
+void logsOff(){
+     app.updateSetting("debugEnabled",[value:"false",type:"bool"])
+}
+
+def mainPage(){
+    dynamicPage (name: "mainPage", title: "", install: true, uninstall: true) {
+      	if (app.getInstallationState() == 'COMPLETE') {   
+          section(name:"userInterface",title:"Iteration Storage UI", hideable: true, hidden: false){
+            paragraph listTable()
+            //input "addSto", "button", title:"Create new Item"
+            if(state.addReq) {
+                state.addReq = false
+                chd = addChildApp("thebearmay", "Device Attribute Iterative Storage - Acquisition", "DAIS ${new Date().getTime()}")
+                paragraph "<script>window.open('http://${location.hub.localIP}:8080/installedapp/configure/${chd.id}/mainPage','#blank')</script>"
+            }
+           
+             
+          }
+          section("Change Application Name", hideable: true, hidden: true){
+            input "nameOverride", "text", title: "New Name for Application", multiple: false, required: false, submitOnChange: true, defaultValue: app.getLabel()
+            if(nameOverride != app.getLabel) app.updateLabel(nameOverride)
+          }
+            section("Debug", hideable:false){
+                input "debugEnabled", "bool", title:"Enable Debug Logging"
+            }
+	    } else {
+		    section("") {
+			    paragraph title: "Click Done", "Please click Done to install app before continuing"
+		    }
+	    }
+    }
+}
+
+String listTable() {
+    ArrayList<String> tHead = ["","Name","Device","Attributes","Interval","Output File","<i style='font-size:1.125rem' class='material-icons he-bin'></i>"]
+    String X = "<i class='he-checkbox-checked'></i>"
+    String O = "<i class='he-checkbox-unchecked'></i>"
+//    List ssList = getSmartList()
+
+//    if (!ssList) { return "$ICON_WARN_O15  <strong>SmartStart List is Empty:</strong> Add to SmartStart using the Edit/Add Page or the Mobile App"}
+
+    String str = "<script src='https://code.iconify.design/iconify-icon/1.0.0/iconify-icon.min.js'></script>"
+    str += "<style>.mdl-data-table tbody tr:hover{background-color:inherit} .tstat-col td,.tstat-col th { padding:8px 8px;text-align:center;font-size:12px} .tstat-col td {font-size:15px }" +
+            "</style><div style='overflow-x:auto'><table class='mdl-data-table tstat-col' style=';border:2px solid black'>" +
+            "<thead><tr style='border-bottom:2px solid black'>"
+    //str += "<th style='font-size:0.8rem !important;border-right:2px solid black'><strong>Enabled</strong></th>" +
+            "<th style='text-align:left'><strong>Name</strong></th>"
+    tHead.each { str += "<th><strong>${it}</strong></th>" }
+    str += "</tr></thead>"
+    
+    getChildApps().each{      
+        str += "<tr><td></td><td><a href='http://${location.hub.localIP}:8080/installedapp/configure/${it.id}/mainPage', target='#blank'>${it.label}</a></td>"
+        str += "<td>${it.getPref('qryDevice')}</td>"
+        attrList = ""
+        i=0
+        it.state.sort().each {
+            if(it.key != "isInstalled" && it.key != "fileCreateReq" && it.key != "rptRestart"){
+                if(i>0) attrList+= ", "
+                attrList += it.key
+                i++
+            }
+        }
+        str += "<td>${attrList}</td>"
+        if(!it.getPref('intType').contains("Value"))
+            wkStr = "${it.getPref('intVal')}${it.getPref('intType').substring(0,1)}"
+        else
+            wkStr = "Value"
+        str += "<td>$wkStr</td>"
+        str += "<td><a href='http://${location.hub.localIP}:8080/local/${it.getPref('stoLocation')}'>${it.getPref('stoLocation')}</a></td>"
+        String remSto = buttonLink("remSto${it.id}", "<i class='material-icons he-bin'></i>", "#ff0000", "6px")
+        str += "<td>$remSto</td></tr>"
+    }
+
+    String addSto = buttonLink("addSto", "<b>＋</b>", "#007009", "25px")
+    str += "<tr><td title='Add new storage definition' style='padding:0px 0px;border:2px solid black'>$addSto</td><td style='color:#007009;font-weight:bold;border:none'>&larr;Add new storage definition</td></tr>"
+    str += "</table></div>"
+
+
+    return str
+}
+
+String buttonLink(String btnName, String linkText, color = "#1A77C9", font = "15px") {
+	"<div class='form-group'><input type='hidden' name='${btnName}.type' value='button'></div><div><div class='submitOnChange' onclick='buttonClick(this)' style='color:$color;cursor:pointer;font-size:$font'>$linkText</div></div><input type='hidden' name='settings[$btnName]' value=''>"
+}
+
+String toCamelCase(init) {
+    if (init == null)
+        return null;
+    init = init.replaceAll("[^a-zA-Z0-9]+","")
+    String ret = ""
+    List word = init.split(" ")
+    if(word.size == 1)
+        return init
+    word.each{
+        ret+=Character.toUpperCase(it.charAt(0))
+        ret+=it.substring(1).toLowerCase()        
+    }
+    ret="${Character.toLowerCase(ret.charAt(0))}${ret.substring(1)}"
+
+    if(debugEnabled) log.debug "toCamelCase return $ret"
+    return ret;
+}
+
+def appButtonHandler(btn) {
+    switch(btn) {    
+        case "addSto":
+            state.addReq = true
+            break
+        case "remSto":
+            state.addReq = true
+            break
+        default: 
+            if (btn.contains("remSto")){
+                cid=btn.substring(6,).toInteger()
+                deleteChildApp(cid)
+                break
+            }
+            log.error "Undefined button $btn pushed"
+            break
+    }
+}

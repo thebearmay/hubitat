b89699ac91e47f071f496ae2b1e4f8e74258a1bf
thebearmay <32334899+thebearmay@users.noreply.github.com>
2024-08-29 15:19:51 -0400

    Update wcDevAudit.groovy

--- apps/wcDevAudit.groovy
+++ apps/wcDevAudit.groovy
@@ -14,7 +14,7 @@
  *    -------------   -------------------    ---------------------------------------------------------
  */
 
-static String version()	{  return '0.0.5'  }
+static String version()	{  return '0.0.6'  }
 import java.security.MessageDigest
 
 definition (
@@ -55,86 +55,46 @@ void logsOff(){
 }
 
 def mainPage(){
-    dynamicPage (name: "mainPage", title: "", install: true, uninstall: true) {
-        section("<b>Errors Found</b>") {
-            childApps = getPistonList()
-            //log.debug "Child apps:${childApps}"
-            wcApp = readPage(state.wcID)
-            //uploadHubFile("wcaWork.txt",wcApp.getBytes('UTF-8'))
-            parDevList = []
-            parDevList2 = []
-            devStart = wcApp.indexOf('/device/edit/')//,wcApp.indexOf('dev:all'))
-            devEnd = wcApp.indexOf('"', devStart)
-            //paragraph "$devStart, $devEnd"
-            i=0
-            while (devEnd > -1 && devStart > -1){
-                if(devStart > -1 && devEnd > -1) {
-                    devNum = wcApp.substring(devStart+13,devEnd).toInteger()
-                    parDevList.add(devNum)
-                    hDnum = md5("core.$devNum")
-                    parDevList2.add(hDnum)
-                }
-                devStart = wcApp.indexOf('/device/edit/',devEnd)
-                devEnd = wcApp.indexOf('"', devStart)
-                i++
+    dynamicPage (name: "mainPage", title: "webCoRE Device Audit", install: true, uninstall: true) {
+        section("") {
+            input("runList", "button", title:"Generate List")
+            if(state.getErr){
+                state.getErr = false
+                paragraph "<h3>Error List</h3><p>${getErrors()}</p>"
+                if(errList.size() < 1) paragraph "No Missing Devices Found"
             }
-            parDevList2.add('ff91564bbd8403bb7f9e5043bb7a9e82')//hsmStatus
-		parDevList2.add('4bb547f511e1fd753f8db83470827793')//Location mode
-            parDevList = parDevList.unique().sort()
-            parDevList2 = parDevList2.unique().sort()
-            //paragraph "Parent List $parDevList"
-            //paragraph "Parent List2 $parDevList2"
-                                    
-            errList = ''
-            childApps.each {
-                i=0
-                //log.debug it.key
-                chdApp = readPage(it.key)
-                uploadHubFile("wcaWork2.txt",chdApp.getBytes('UTF-8'))
-                devStart = chdApp.indexOf('/device/edit/')
-                devEnd = chdApp.indexOf('"', devStart)
-                devList = []
-                while (devEnd > -1 && devStart > -1){
-                    if(devStart > -1 && devEnd > -1) {
-                        devChk = chdApp.substring(devStart+13,devEnd).toInteger()
-                        if(!parDevList.contains(devChk)){
-                            dName = chdApp.substring(devEnd+2,chdApp.indexOf('<',devEnd+2))
-                            errList +="Piston <a href='http://${location.hub.localIP}/installedapp/status/${it.key}'>${it.value}</a> is missing device <b>$dName</b>\n"
-                        }
-                    }
-                    devList.add(devChk)
-                    devStart = chdApp.indexOf('/device/edit/',devEnd)
-                    devEnd = chdApp.indexOf('"', devStart)
-                    i++
-                }             
-                devEnd = chdApp.indexOf('not found}')
-                devStart = chdApp.indexOf('Device', devEnd-70)
-                i=0
-                while (devEnd > -1 && devStart > -1){
-                    dMsg = 'd'+chdApp.substring(devStart+1,devEnd+9)
-                    errList +="Piston <a href='http://${location.hub.localIP}/installedapp/status/${it.key}'>${it.value}</a> <b>$dMsg</b>\n"
-                    devEnd = chdApp.indexOf('not found}',devStart+71)
-                    devStart = chdApp.indexOf('Device', devEnd-70)
-                    i++
+
+        }
+        section("Exclusions Maintenance",hideable:true,hidden:true){
+            if(!state.exclude) state.exclude = [:]
+            else{
+                paragraph "<h3>Excluded Hashes</h3>"
+                state.exclude.each {
+                    paragraph "${it.key}::${it.value}"
                 }
-                devStart=chdApp.indexOf('d&#x3D;:')
-                devEnd=chdApp.indexOf(':',devStart+8)
-                //if(devEnd > -1 && devStart > -1)
-                //    log.debug "${it.value}: ${chdApp.substring(devStart+8,devEnd)}"
-                i=0
-                while (devEnd > -1 && devStart > -1){
-                    hashVal = chdApp.substring(devStart+8,devEnd)
-                    if(!parDevList2.contains(hashVal))
-                        errList +="Piston <a href='http://${location.hub.localIP}/installedapp/status/${it.key}'>${it.value}</a> contains unknown device <b>':$hashVal:'</b>\n"
-                    devStart=chdApp.indexOf('d&#x3D;:',devEnd)
-                    devEnd=chdApp.indexOf(':',devStart+8)
-                    i++
-                }                
-
             }
-            if(errList.size() < 1) errList = "No Missing Devices Found"
-            paragraph errList
-
+            input("excName", "text", title:"<b>Name of Exclusion<b>")
+            input("excHash", "text", title:"<b>Hash Value to Exclude</b>")
+            input("addExc","button",title:"Add", size:4)
+            if(state.addExclusion) {
+                state.addExclusion = false
+                excName = toCamelCase(excName)
+                state.exclude["$excName"]="$excHash"
+                app.removeSetting("excName")
+                app.removeSetting("excHash")
+            }
+            input("delExc","button",title:"Delete",size:4)
+            if(state.delExclusion) {
+                state.delExclusion = false
+                excName = toCamelCase(excName)
+                wMap = state.exclude
+                state.exclude = [:]
+                wMap.each{
+                    if(it.key != excName)
+                    state.exclude["${it.key}"]="${it.value}"
+                }
+                app.removeSetting("excName")
+            }
         }
     }
 }
@@ -161,6 +121,88 @@ String readPage(pId){
     }
 }
 
+String getErrors(){
+    childApps = getPistonList()
+    //log.debug "Child apps:${childApps}"
+    wcApp = readPage(state.wcID)
+    //uploadHubFile("wcaWork.txt",wcApp.getBytes('UTF-8'))
+    parDevList = []
+    parDevList2 = []
+    devStart = wcApp.indexOf('/device/edit/')//,wcApp.indexOf('dev:all'))
+    devEnd = wcApp.indexOf('"', devStart)
+    //paragraph "$devStart, $devEnd"
+    i=0
+    while (devEnd > -1 && devStart > -1){
+        if(devStart > -1 && devEnd > -1) {
+            devNum = wcApp.substring(devStart+13,devEnd).toInteger()
+            parDevList.add(devNum)
+            hDnum = md5("core.$devNum")
+            parDevList2.add(hDnum)
+        }
+        devStart = wcApp.indexOf('/device/edit/',devEnd)
+        devEnd = wcApp.indexOf('"', devStart)
+        i++
+    }
+    if(state.exclude) {
+        state.exclude.each{
+            //parDevList2.add('ff91564bbd8403bb7f9e5043bb7a9e82')//hsmStatus
+            parDevList2.add(it.value.toString())
+        }
+    }
+    parDevList = parDevList.unique().sort()
+    parDevList2 = parDevList2.unique().sort()
+    //paragraph "Parent List $parDevList"
+    //log.debug "Parent List2 $parDevList2"
+                                    
+    errList = ''
+    childApps.each {
+        i=0
+        //log.debug it.key
+        chdApp = readPage(it.key)
+        //uploadHubFile("wcaWork2.txt",chdApp.getBytes('UTF-8'))
+        devStart = chdApp.indexOf('/device/edit/')
+        devEnd = chdApp.indexOf('"', devStart)
+        devList = []
+        while (devEnd > -1 && devStart > -1){
+            if(devStart > -1 && devEnd > -1) {
+                devChk = chdApp.substring(devStart+13,devEnd).toInteger()
+                if(!parDevList.contains(devChk)){
+                    dName = chdApp.substring(devEnd+2,chdApp.indexOf('<',devEnd+2))
+                    errList +="Piston <a href='http://${location.hub.localIP}/installedapp/status/${it.key}'>${it.value}</a> is missing device <b>$dName</b>\n"
+                }
+            }
+            devList.add(devChk)
+            devStart = chdApp.indexOf('/device/edit/',devEnd)
+            devEnd = chdApp.indexOf('"', devStart)
+            i++
+        }             
+        devEnd = chdApp.indexOf('not found}')
+        devStart = chdApp.indexOf('Device', devEnd-70)
+        i=0
+        while (devEnd > -1 && devStart > -1){
+            dMsg = 'd'+chdApp.substring(devStart+1,devEnd+9)
+            errList +="Piston <a href='http://${location.hub.localIP}/installedapp/status/${it.key}'>${it.value}</a> <b>$dMsg</b>\n"
+            devEnd = chdApp.indexOf('not found}',devStart+71)
+            devStart = chdApp.indexOf('Device', devEnd-70)
+            i++
+        }
+        devStart=chdApp.indexOf('d&#x3D;:')
+        devEnd=chdApp.indexOf(':',devStart+8)
+        //if(devEnd > -1 && devStart > -1)
+        //    log.debug "${it.value}: ${chdApp.substring(devStart+8,devEnd)}"
+        i=0
+        while (devEnd > -1 && devStart > -1){
+            hashVal = chdApp.substring(devStart+8,devEnd)
+            if(!parDevList2.contains(hashVal))
+                errList +="Piston <a href='http://${location.hub.localIP}/installedapp/status/${it.key}'>${it.value}</a> contains unknown device <b>':$hashVal:'</b>\n"
+                devStart=chdApp.indexOf('d&#x3D;:',devEnd)
+                devEnd=chdApp.indexOf(':',devStart+8)
+                i++
+        }                
+
+    }
+    return errList
+}
 
 ArrayList getPistonList() {
     Map requestParams =
@@ -192,8 +234,43 @@ String md5(String md5){
 	byte[] array=md.digest(md5.getBytes())   
 	String r= ''
 	Integer l=array.size()
-	for(Integer i=0; i<l;++i){
+	for(Integer i=0; i<l; ++i){
 		r+=Integer.toHexString((array[i] & 0xFF)| 0x100).substring(1,3)
 	}
 	return r
+}
+
+String toCamelCase(init) {
+    if (init == null)
+        return null;   
+	
+    String ret = ""
+    List word = init.split(" ")
+    if(word.size == 1)
+        return init
+    word.each{
+        ret+=Character.toUpperCase(it.charAt(0))
+        ret+=it.substring(1).toLowerCase()        
+    }
+    ret="${Character.toLowerCase(ret.charAt(0))}${ret.substring(1)}"
+
+    if(debugEnabled) log.debug "toCamelCase return $ret"
+    return ret;
+}
+    
+def appButtonHandler(btn) {
+    switch(btn) {
+        case "runList":
+            state.getErr = true
+            break
+        case "addExc":
+            state.addExclusion = true
+            break
+        case "delExc":
+            state.delExclusion = true
+            break
+        default: 
+              log.error "Undefined button $btn pushed"
+              break
+    }
 }

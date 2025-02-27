4915a152e6c39dd6d4e91263be0cda5600905260
thebearmay <32334899+thebearmay@users.noreply.github.com>
2025-02-27 17:01:48 -0500

    Update custDevNote.groovy

--- apps/custDevNote.groovy
+++ apps/custDevNote.groovy
@@ -21,10 +21,11 @@
  *    21Oct2024                   2.0.6 - Change buttons on maintenance pages
  *    08Nov2024                   2.0.7 - Left Align device name
  *    17Nov2024                   2.0.8 - button styling
+ *	  27Feb2025					  2.0.9 - code cleanup
  */
 import groovy.transform.Field
-static String version()	{  return '2.0.8'  }
-String appLocation() { return "http:${location.hub.localIP}/installedapp/configure/${app.id}/mainPage" }
+static String version()	{  return '2.0.9'  }
+String appLocation() { return "http://${location.hub.localIP}/installedapp/configure/${app.id}/mainPage" }
 
 
 definition (
@@ -75,9 +76,6 @@ def mainPage(){
 				if(nameOverride != app.getLabel()) app.updateLabel(nameOverride)				
             }
             section(""){
-                fileList = getFiles()
-                if(!fileList.contains('iconify'))
-                   fetchJS()
                 settings.each{
                     if("${it.key}".contains("sdKey")){
                         app.removeSetting("${it.key}")
@@ -87,11 +85,12 @@ def mainPage(){
                 paragraph buildDeviceTable() 
                 if(state.singleDev){
                     state.singleDev = false
-                    paragraph "<script>window.location.replace('${appLocation()}/noteMaint')</script>"
+                    inx = appLocation().lastIndexOf("/")
+                    paragraph "<script>window.location.replace('${appLocation().substring(0,inx)}/noteMaint')</script>"
                 }                    
                 
                 //href name: "noteMaintHref", page: "noteMaint",title: "${btnIcon('pi-pencil')} Note Maint", description: "", width: 4, newLine: false//, params:[did: 30]
-                                
+                         
 
 	       }        
 	    } else {
@@ -106,8 +105,15 @@ String buttonLink(String btnName, String linkText, color = "#1A77C9", font = "15
 	"<div class='form-group'><input type='hidden' name='${btnName}.type' value='button'></div><div><div class='submitOnChange' onclick='buttonClick(this)' style='color:$color;cursor:pointer;font-size:$font'>$linkText</div></div><input type='hidden' name='settings[$btnName]' value=''>"
 }
 
-String buttonLink2(String btnName, String linkText, color = "#1A77C9", bkColor = "#FFFFFF", font = "15px") {
-	"<div class='form-group'><input type='hidden' name='${btnName}.type' value='button'></div><div><div class='submitOnChange' onclick='buttonClick(this)' style='border-radius:25px;color:$color;background-color:$bkColor;cursor:pointer;font-size:$font; border-style:outset;width:10em;'>$linkText</div></div><input type='hidden' name='settings[$btnName]' value=''>"
+String buttonLink2(HashMap opt) { 
+    if(!opt.name || !opt.title ) 
+    	return "Error missing name or title"
+    if(!opt.color) opt.color = "#1A77C9"
+    if(!opt.background) opt.background = "#FFFFFF"
+    if(!opt.fontSize) opt.fontSize = "15px"
+    if(!opt.width) opt.width = '10em'
+    if(!opt.radius) opt.radius = '25px'
+	return "<div class='form-group'><input type='hidden' name='${opt.name}.type' value='button'></div><div><div class='submitOnChange' onclick='buttonClick(this)' style='border-radius:25px;color:${opt.color};background-color:${opt.background};cursor:pointer;font-size:${opt.fontSize}; border-style:outset;width:${opt.width};'>${opt.title}</div></div><input type='hidden' name='settings[${opt.name}]' value=''>"
 }
 
 String btnIcon(String name) {
@@ -122,7 +128,7 @@ String buildDeviceTable(){
     String removeIcon = "<i class='material-icons he-bin'></i>"
 
 
-    String str = "<script src='/local/iconify-icon.min.js'></script>$ttStyleStr"
+    String str = "$ttStyleStr"//<script src='/local/iconify-icon.min.js'></script>
     str += "$tableStyle<div style='overflow-x:auto'><table class='mdl-data-table tstat-col' style='border-left:2px solid black;border-top:2px solid black;'>" +
             "<thead><tr style='border-bottom:2px solid black'>"
     tHead.each { str += "<th><strong>${it}</strong></th>" }
@@ -158,7 +164,8 @@ String buildDeviceTable(){
         str += "<td>$devSel</td><td style='text-align:left'><a href='http://${location.hub.localIP}/device/edit/${it.id}' target='_self'>${devName}</a><td class='tTip'>$hoverList$noteList</td>"
         str += "</tr>"
     }
-    String addNote = "<a href='${appLocation()}/noteMaint' target='_self' style='color:#007009;font-size:25px;'><b>+</b></a>"
+    inx = appLocation().lastIndexOf("/")
+    String addNote = "<a href='${appLocation().substring(0,inx)}/noteMaint' target='_self' style='color:#007009;font-size:25px;'><b>+</b></a>"
     //buttonLink("addNote", "<b>＋</b>", "#007009", "25px")
     str += "<tr style='border-top:2px solid black;border-right:none'><td title='Add/Edit/Remove Note' style='padding:0px 0px;border:2px solid black'>$addNote</td><td colspan='6' style='color:#007009;font-weight:bold;border:none;text-align:left'>&larr;Add/Edit/Remove Note</td></tr>"
     str += "</table></div>"
@@ -166,7 +173,7 @@ String buildDeviceTable(){
 }
 
 def noteMaint(){
-    dynamicPage (name:"noteMaint", title: "<h2 style='color:blue'>Custom Note <span style='font-size: x-small'>v${version()}</span></h2>", install: false, uninstall: false, nextPage:mainPage) {
+    dynamicPage (name:"noteMaint", title: "<h2 style='color:blue'>Custom Note <span style='font-size: x-small'>v${version()}</span></h2>", install: false, uninstall: false) {
         dList = getDevList()
         if(dList.devList.size() <= 0){
             section("") {
@@ -175,12 +182,10 @@ def noteMaint(){
         }else if(dList.devList.size() == 1) {
             section("<h4>Single Device Maintenance<h4>", hideable:false, hidden: false){
                 paragraph "<span style='background-color:yellow;font-weight:bold'>Selected device: ${dList.devListName[0]}</span>"
-                saveBtn = buttonLink2("sdSave", "<b>Save</b>", "#FFFFFF", "#007000", "15px")
-                remBtn = buttonLink2("sdRem", "<b>Remove</b>", "#FFFFFF", "#700000", "15px")
+                saveBtn = buttonLink2([name:"sdSave", title:"<b>Save</b>", color:"#FFFFFF", background:"#007000", fontSize:"15px"])
+                remBtn = buttonLink2([name:"sdRem", title:"<b>Remove</b>", color:"#FFFFFF", background:"#700000", fontSize:"15px"])
                 paragraph "<style>td{text-align:center}</style><table><tr><td>$saveBtn</td><td>$remBtn</td></tr></table>"
-                //input "sdSave", "button", title:"<b>Save</b>", width:2, backgroundColor:'#007000',textColor:'#ffffff'
-                //input "sdRem", "button", title:"<b>Remove</b>", width:2, backgroundColor:'#700000',textColor:'#ffffff'
-                //input "hidden","hidden", title:"", width:8
+
                 input "newKey", "text", title:"<b style='background-color:#87CECB'>New/Remove Key</b>",submitOnChange:true, width:6
                 if(newKey) app.updateSetting("newKey",[value:"${toCamelCase(newKey)}",type:"text"])
                 input "newVal", "text", title:"<b style='background-color:#87CECB'>New Note</b>",submitOnChange:true, width:6                               
@@ -240,12 +245,10 @@ def noteMaint(){
         } else {
             section("<h4>Multi-Device Maintenance</h4>", hideable:false, hidden: false){
                 paragraph "<span style='background-color:yellow;font-weight:bold'>Selected devices: ${dList.devListName.sort()}</span>"
-                saveBtn = buttonLink2("mdSave", "<b>Save</b>", "#FFFFFF", "#007000", "15px")
-                remBtn = buttonLink2("mdRem", "<b>Remove</b>", "#FFFFFF", "#700000", "15px")
+                saveBtn = buttonLink2([name:"mdSave", title:"<b>Save</b>", color:"#FFFFFF", background:"#007000", fontSize:"15px"])
+                remBtn = buttonLink2([name:"mdRem", title:"<b>Remove</b>", color:"#FFFFFF", background:"#700000", fontSize:"15px"])
                 paragraph "<style>td{text-align:center}</style><table><tr><td>$saveBtn</td><td>$remBtn</td></tr></table>"
-//                input "mdSave", "button", title:"<b>Save</b>", width:2, backgroundColor:'#007000',textColor:'#ffffff'
-//                input "mdRem", "button", title:"<b>Remove</b>", width:2, backgroundColor:'#700000',textColor:'#ffffff'
-//                input "hidden","hidden", title:"", width:8
+
                 input "newKey", "text", title:"<b style='background-color:#87CECB'>New/Remove/Update Key</b>",submitOnChange:true, width:6
                 if(newKey) app.updateSetting("newKey",[value:"${toCamelCase(newKey)}",type:"text"])
                 input "newVal", "text", title:"<b style='background-color:#87CECB'>New/Updated Note</b>",submitOnChange:true, width:6                               
@@ -327,12 +330,6 @@ def appButtonHandler(btn) {
     if(debugEnabled)log.debug "$btn pressed"
     switch(btn) {
 	case "addNote":
-/*            if(it.controllerType == "LNK") {
-                atomicState.meshedDeviceMsg+="<span style='background-color:red;font-weight:bold;color:white;'>$it is a Hub Mesh Device, note must be added to the <i>REAL</i> device to be retained</span><br>"
-            }
-		}
-        if(atomicState.meshedDeviceMsg == "") atomicState.meshedDeviceMsg = "<span style='background-color:green;font-weight:bold;color:white;'>Update Successful</span>"
-*/
         state.addNote = true
 		break
 	case "remNote":
@@ -377,57 +374,6 @@ def appButtonHandler(btn) {
 		    log.error "Undefined button $btn pushed"
 		break
 	}
-}
-
-ArrayList getFiles(){
-    fileList =[]
-    params = [
-        uri    : "http://127.0.0.1:8080",
-        path   : "/hub/fileManager/json",
-        headers: [
-            accept : "application/json"
-        ],
-    ]
-    httpGet(params) { resp ->
-        resp.data.files.each {
-            fileList.add(it.name)
-        }
-    }
-    
-    return fileList.sort()
-}
-
-String readExtFile(fName){  
-    def params = [
-        uri: fName,
-        contentType: "text/html",
-        textParser: true  
-    ]
-
-    try {
-        httpGet(params) { resp ->
-            if(resp!= null) {
-                return """${resp.data}"""
-            }
-            else {
-                log.error "Read External - Null Response"
-                return null
-            }
-        }
-    } catch (exception) {
-        log.error "Read Ext Error: ${exception.message}"
-        return null
-    }
-}
-
-
-def fetchJS(){
-    jsFile = readExtFile("https://raw.githubusercontent.com/thebearmay/hubitat/main/libraries/iconify-icon.min.js")
-    if(jsFile){
-        bArray = (jsFile.getBytes("UTF-8"))
-	    uploadHubFile("iconify-icon.min.js",bArray)
-    } else
-        log.error "iconify-icon.min.js not found"
 }
 
 def intialize() {

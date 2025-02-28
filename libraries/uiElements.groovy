66a3ef5fc5f014087dc4793797dec99453a1dad4
thebearmay <32334899+thebearmay@users.noreply.github.com>
2025-02-28 10:41:41 -0500

    Update uiElements.groovy

--- libraries/uiElements.groovy
+++ libraries/uiElements.groovy
@@ -13,12 +13,26 @@
 
 
 
-String input3 = inputItem([name:"testItem4", type:"number", title:'Input Title3', defaultValue:12, width:'20em', background:'green',color:'white'])
-paragraph input3
-...
+
+/*****************************************************************************
+* Returns a string that will create an input element for an app - limited to *
+* text and number inputs currently                                           *
+*                                                                            *
+* HashMap fields:                                                            *
+*	name - (required) name to store the input as a setting, no spaces or *
+*		special characters					     *
+*	type - (required) input type, 'text' or 'number' only 		     *
+*	title - displayed description for the input element		     * 
+*	width - CSS descriptor for field width				     *
+*	background - CSS color descriptor for the input background color     *
+*	color - CSS color descriptor for text color			     *
+*	fontSize - CSS text size descriptor				     *
+*	multiple - true/<false>						     *
+*	defaultValue - default for the field				     *
+*****************************************************************************/
 
 String inputItem(HashMap opt) {
-	if(!opt.name || !opt.type) return "Error missing name or type"
+    if(!opt.name || !opt.type) return "Error missing name or type"
     if(opt.type != 'number' && opt.type != 'text'){
         log.warn "${opt.type} type not currently supported - using text"
         opt.type = 'text'
@@ -28,6 +42,7 @@ String inputItem(HashMap opt) {
     if(opt.width) computedStyle += "width:${opt.width};"
     if(opt.background) computedStyle += "background-color:${opt.background};"
     if(opt.color) computedStyle += "color:${opt.color};"
+    if(opt.fontSize) computedStyle += "font-size:${opt.fontSize};"
     String retVal = "<div class='form-group'><input type='hidden' name='${opt.name}.type' value='${opt.type}'><input type='hidden' name='${opt.name}.multiple' value='false'></div>"
 	retVal+="<div class='mdl-cell mdl-cell--4-col mdl-textfield mdl-js-textfield has-placeholder is-dirty is-upgraded' style='' data-upgraded=',MaterialTextfield'>"
 	retVal+="<label for='settings[$opt.name]' class='control-label'>$opt.title</label><div class='flex'><input type='$opt.type' name='settings[$opt.name]' class='mdl-textfield__input submitOnChange' style='$computedStyle' value='$opt.defaultValue' placeholder='Click to set' id='settings[$opt.name]'>"
@@ -35,21 +50,47 @@ String inputItem(HashMap opt) {
     return retVal
 }
 
+/*****************************************************************************
+* Returns a string that will create an button element for an app 	     *
+*                                                                            *
+* HashMap fields:                                                            *
+*	name - (required) name to identify the button, no spaces or 	     *
+*		special characters					     *
+*	title - (required) button label					     *
+*	width - CSS descriptor for field width				     *
+*	background - CSS color descriptor for the input background color     *
+*	color - CSS color descriptor for text color			     *
+*	fontSize - CSS text size descriptor				     *
+*	radius - CSS border radius descriptor (corner rounding)		     *
+*****************************************************************************/
+
 String buttonLink(HashMap opt) { 
     if(!opt.name || !opt.title ) 
     	return "Error missing name or title"
-    if(!opt.color) opt.color = "#1A77C9"
-    if(!opt.background) opt.background = "#FFFFFF"
-    if(!opt.fontSize) opt.fontSize = "15px"
-    if(!opt.width) opt.width = '10em'
-    if(!opt.radius) opt.radius = '25px'
-    return "<div class='form-group'><input type='hidden' name='${opt.name}.type' value='button'></div><div><div class='submitOnChange' onclick='buttonClick(this)' style='border-radius:25px;color:${opt.color};background-color:${opt.background};cursor:pointer;font-size:${opt.fontSize}; border-style:outset;width:${opt.width};'>${opt.title}</div></div><input type='hidden' name='settings[${opt.name}]' value=''>"
+    String computedStyle = ''
+    if(opt.width) computedStyle += "width:${opt.width};"
+    if(opt.background) computedStyle += "background-color:${opt.background};"
+    if(opt.color) computedStyle += "color:${opt.color};"
+    if(opt.fontSize) computedStyle += "font-size:${opt.fontSize};"
+    if(opt.radius) computedStyle += "border-radius:${opt.radius};"
+    return "<div class='form-group'><input type='hidden' name='${opt.name}.type' value='button'></div><div><div class='submitOnChange' onclick='buttonClick(this)' style='$computedStyle'>${opt.title}</div></div><input type='hidden' name='settings[${opt.name}]' value=''>"
 }
+
+/*****************************************************************************
+* Returns a string that will create an button icon element for an app from   *
+*	the materials-icon font						     *
+*                                                                            *
+*	name - (required) name of the icon to create			     *
+*****************************************************************************/
 
 String btnIcon(String name) {
     return "<span class='p-button-icon p-button-icon-left pi " + name + "' data-pc-section='icon'></span>"
 }
 
+
+/*****************************************************************************
+* Code sample that returns a string that will create a standard HE table     *
+*****************************************************************************/
 
 String listTable() {
     ArrayList<String> tHead = ["","Disable","Name","Device","Attributes","Interval","Output File","<i style='font-size:1.125rem' class='material-icons he-bin'></i>"]

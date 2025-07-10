25e429f0312a63077ee04c39b45119016cae9aae
thebearmay <32334899+thebearmay@users.noreply.github.com>
2025-07-10 12:30:44 -0400

    Update lockHistory.groovy

--- apps/lockHistory.groovy
+++ apps/lockHistory.groovy
@@ -19,7 +19,8 @@
  *    2021-05-04    thebearmay    2.2.7.x changes
  *    2021-12-28    thebearmay    return State as a map
  *    2022-06-20    thebearmay    remove embedded sections
- *    2023-05-16    thebearmat    allow leading zeros in code
+ *    2023-05-16    thebearmay    allow leading zeros in code
+ *	  2025-07-10	thebearmay	  allow alternate names for lock devices
  */
 
 import java.text.SimpleDateFormat
@@ -43,6 +44,7 @@ preferences {
    page name: "mainPage"
    page name: "lockHistory"
    page name: "altName"
+   page name: "altLockName"
 }
 
 def installed() {
@@ -74,7 +76,9 @@ def mainPage(){
                 input "qryDate", "string", title: "Pull event data from this date forward (yyyy-MM-dd hh:mm):", required: true, submitOnChange: true
                 if (qryDevice != null && qryDate != null) href "lockHistory", title: "Lock History", required: false
                 href "altName", title: "Maintain Alternate Names for events that return 'unknown codeNumber:x' or 'code #x'", required: false
+                href "altLockName", title: "Maintain Alternate Names for lock devices", required: false
                 input "notifyDevice", "capability.notification", title: "Notification Devices:", multiple: true, submitOnChange: true
+                input "ignorePhysical", "bool", title: "Only report unlock events from digital events"
                 if(notifyDevice?.size() > 0) 
                     lockSubscribe()
                 else
@@ -99,44 +103,47 @@ def lockSubscribe(){
 def lockHandler(evt){ 
     if(evt.value == "unlocked") {
         notifyDevice.each {
+            evtDev = evt.getDevice()
+            if(settings["lock${evt.deviceId}"]) evtDev = settings["lock${evt.deviceId}"]
             if(evt.descriptionText.contains("unknown codeNumber:") || evt.descriptionText.contains("code #")){
-                evtDev = evt.getDevice()
                 altNam = findAltName(evt.descriptionText)
                 it.deviceNotification("$evtDev was unlocked by $altNam")
+            } else if(ignorePhysical && evt.type != 'digital') {
+                log.info "$evtDev physical unlock event notification skipped"
             } else
-                it.deviceNotification(evt.descriptionText)
+                evtDesc = evt.descriptionText.substring(evt.descriptionText.indexOf("was"))
+                it.deviceNotification("$evtDev ${evtDesc}")
         }
     }
 }
 
 def lockHistory(){
-    dynamicPage (name: "lockHistory", title: "", install: false, uninstall: false) {
+    dynamicPage (name: "lockHistory", title: "", install: false, uninstall: false, nextPage:"mainPage") {
         section("Lock History"){          
           dispTable = buildTable()
           paragraph  "$dispTable"  
         }
  
         section ("Lock Details", hideable: true, hidden: false) {  
-                input  "codeRec", "bool", title:"Last Code Records", defaultValue: true, submitOnChange:true
-                input  "unlockRec", "bool", title:"Unlock Records", defaultValue: true, submitOnChange: true
-                input  "lockRec", "bool", title:"Lock Records", defaultValue: true, submitOnChange: true
-                input "qryDate", "string", title: "Pull event data from this date forward (yyyy-MM-dd hh:mm):", required: true, submitOnChange: true
+                input  "codeRec", "bool", title:"Last Code Records", defaultValue: true, submitOnChange:true, width:4
+                input  "unlockRec", "bool", title:"Unlock Records", defaultValue: true, submitOnChange: true, width:4
+                input  "lockRec", "bool", title:"Lock Records", defaultValue: true, submitOnChange: true, width:4
+                input "qryDate", "string", title: "Pull event data from this date forward (yyyy-MM-dd hh:mm):", required: true, submitOnChange: true, width:8
                 input "refreshTable", "button", title:"Refresh Table"
-            }
-            section (""){    
-                href "mainPage", title: "Return", required: false
             }
       
     }
 }
 
 String buildTable(){
-    dispTable = "<style type='text/css'>div{overflow:auto;} .mtable {border:1px black solid;padding:0px;width:100%;}.mth {border:1px black solid; min-width:16em;} .mtd {border:1px black solid;vertical-align:text-top;}</style><div class='.mdiv'><table class='mtable'><tr>"
+    dispTable = "<style type='text/css'>.mtable {border:1px black solid;padding:0px;width:100%;}.mth {border:1px black solid; min-width:16em;} .mtd {border:1px black solid;vertical-align:text-top;}</style><div class='.mdiv'><table class='mtable'><tr>"
     evtList = {}
     evtArr = []
     for(i=0;i<qryDevice.size();i++){
-        evtArr[i]=[]
-        dName = qryDevice[i].label ?: qryDevice[i].name 
+        evtArr[i]=[]     
+        dName = qryDevice[i].displayName
+        if(settings["lock${qryDevice[i].deviceId}"]) 
+        	dName = settings["lock${qryDevice[i].deviceId}"]
         dispTable+="<th class='mth'> $dName </th>"
         def evtList = [] 
         if(codeRec) evtList+=qryDevice[i].statesSince("lastCodeName",Date.parse("yyyy-MM-dd hh:mm", qryDate),[max:100])
@@ -188,8 +195,19 @@ String findAltName(unkStr){
     else return "Key Not Found: $result"
 }
 
+def altLockName(){
+    dynamicPage (name: "altLockName", title: "", install: false, uninstall: false, nextPage:"mainPage") {
+        section("Alternate Lock Names"){   
+            qryDevice.each {
+                input "lock${it.deviceId}", "string", title:"Alternate for <b>${it.displayName}</b>", defaultValue:"$it.displayName", width:4, submitOnChange:true
+            }
+        }
+    }
+}
+
+
 def altName(){
-    dynamicPage (name: "altName", title: "", install: false, uninstall: false) {
+    dynamicPage (name: "altName", title: "", install: false, uninstall: false, nextPage:"mainPage") {
         section("Alternate Names"){   
             dispTable = buildNames()
                 paragraph dispTable
@@ -213,9 +231,6 @@ def altName(){
                 paragraph "<script>location.reload();</script>"
             }
                     
-        }
-        section (""){   
-            href "mainPage", title: "Return", required: false
         }
     }
 }

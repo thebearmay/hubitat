7ec14d1de2396bc4c5fe20aef3b05c01ad5f7e7b
thebearmay <32334899+thebearmay@users.noreply.github.com>
2026-04-24 11:55:50 -0400

    Update usefulMethods.groovy

--- libraries/usefulMethods.groovy
+++ libraries/usefulMethods.groovy
@@ -2,12 +2,15 @@
 // Find namespace based on driver name - works in both apps and drivers
 
 String findNameSpace(String driverName) {
-  String nSpace = ''
+	String nSpace = ''
 	this.installedDrivers.each{
-    if(it.name == driverName) nSpace = it.namespace
-  }
-  return nSpace
+    	if(it.name == driverName) nSpace = it.namespace
+  	}
+	return nSpace
 }
 
 this.definitionData -> the fields in the app defintion
 this.hubFiles -> list of files with size, date, etc. (both apps and drivers)
+this.installedDrivers
+this.rooms 
+this.installedBundlesList

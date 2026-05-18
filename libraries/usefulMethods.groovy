bfc70a08e3d82536531eef869d42d45b75d5db68
thebearmay <32334899+thebearmay@users.noreply.github.com>
2026-05-18 09:10:46 -0400

    Update usefulMethods.groovy

--- libraries/usefulMethods.groovy
+++ libraries/usefulMethods.groovy
@@ -9,8 +9,21 @@ String findNameSpace(String driverName) {
 	return nSpace
 }
 
+Boolean fileExists(fName) {
+    Boolean fExist = false
+    this.hubFiles.each{
+        if(it.name == fName)
+        	fExist = true
+    }
+    return fExist
+}
+
+
+
 this.definitionData -> the fields in the app defintion
-this.hubFiles -> list of files with size, date, etc. (both apps and drivers)
+this.hubFiles -> list of files with size, date, id, type[file,dir] (both apps and drivers)
+this.installedBundlesList -> list of of bundles with id,name,namespace
 this.installedDrivers
 this.rooms 
-this.installedBundlesList
+this.TTSVoices -> name,gender,language
+this.wiFiNetworks
